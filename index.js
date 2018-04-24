761696957c0b34fcc2973ddd9e326a8095e3a261
Gregory Schmitt <gjschmitt@live.com>
2018-04-24 22:36:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,62 @@
-function takeANumber(katzDeliLine, newPerson) {
-  katzDeliLine.push(newPerson);
-  return("Welcome, " + newPerson + ". You are number " + (katzDeliLine.length) + " in line.");
+var countCustomersToday = 0;
+var customers = [];
+var hourlyTraffic = [];
+
+function runGregLab() {
+  
+  takeANumber(customers);
+  takeANumber(customers);
+  takeANumber(customers);
+  
+  console.log(customers)
+  
+  console.log(hourlyTraffic)
+  
+  console.log(findBusiestHour());
+  
+  hourlyTraffic[11] = 5;  // pretend that 11am is busiest hour
+  
+  console.log(findBusiestHour());
+  
+}
+
+
+
+function findBusiestHour() {
+  var i;
+  var busiestHour = 0;
+  var maxTraffic = 0;
+  
+  for (i=1, len = hourlyTraffic.length; i < len; i++) {
+    
+    if (hourlyTraffic[i] === null || hourlyTraffic[i] === undefined) continue;
+    if (hourlyTraffic[i] > maxTraffic) {
+      maxTraffic = hourlyTraffic[i];
+      busiestHour = i;
+    } 
+    else {
+      maxTraffic = hourlyTraffic[i];
+    }
+  }  
+  return busiestHour;
+}
+
+
+function takeANumber(katzDeliLine) {
+  var thisHour;
+  var d = new Date();
+  
+  countCustomersToday++;
+  katzDeliLine.push (countCustomersToday)
+  
+  thisHour = d.getHours(); 
+  if (!hourlyTraffic[thisHour]) {
+    hourlyTraffic[thisHour] = 1;
+  } else {
+    hourlyTraffic[thisHour] = hourlyTraffic[thisHour] + 1;  
+  }
+
+  return countCustomersToday; 
 }
 
 function nowServing(katzDeliLine) {
@@ -9,7 +65,9 @@ function nowServing(katzDeliLine) {
   if (katzDeliLine.length === 0) {
     return("There is nobody waiting to be served!");
   } else {
+    
     katzDeliLine.shift();
+    
     return("Currently serving " + firstPerson + ".");
   }
     
